74daeecffe047aa4c3d827aae7b7dfb001fea8db
Administrator <olga.v.nosova@gmail.com>
2017-03-16 21:02:00 -0400

    ag-grid styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import {AuthGuard} from './common/auth-guard';
 import { ContactComponent } from './contact/contact.component';
 import { HelpComponent } from './help/help.component';
+import { MyGridApplicationComponent } from './my-grid-application/my-grid-application.component';
 import { LoginComponent } from './login/login.component';
 
 export const rootRouterConfig: Routes = [
@@ -10,6 +11,7 @@ export const rootRouterConfig: Routes = [
   { path: 'contact', component: ContactComponent,  data: { title: 'Heroes List', activeTab: 'contact' }},
   { path: 'contact/:org', component: ContactComponent,  data: {activeTab: 'contact'}},
   { path: 'help', component: HelpComponent,  canActivate: [AuthGuard], data: {activeTab: 'help'} },
+  { path: 'grid', component: MyGridApplicationComponent,  data: {activeTab: 'grid'} },
   { path: 'login/:returnUrl', component: LoginComponent, data: {activeTab: 'login'}},
   { path: 'login', component: LoginComponent, data: {activeTab: 'login'}},
   { path: '**', component: LoginComponent }
